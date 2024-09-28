bc36248d400d1e27b423b145b259ad61329ff0ef
Jeromos Kovacs <iitsnotme214@proton.me>
2024-09-28 19:39:26 +0200

    fix: new shapes can very easily be added

--- src/app.rs
+++ src/app.rs
@@ -15,7 +15,7 @@ mod cell;
 /// Keymaps to handle input events
 mod kmaps;
 /// Starting shapes
-mod shapes;
+pub mod shapes;
 /// ui
 mod ui;
 /// Conway's Game of Life universe
@@ -24,7 +24,113 @@ mod universe;
 #[cfg(test)]
 mod tests;
 
+pub struct App {
+    pub available_universes: Vec<Universe>,
+    universe: Universe,
+    i: usize,
+    pub poll_t: Duration,
+    pub paused: bool,
+    pub area: Area,
+}
+impl Default for App {
+    fn default() -> Self {
+        App {
+            area: Area::default(),
+            universe: Universe::default(),
+            i: 0,
+            poll_t: DEF_DUR,
+            paused: false,
+            available_universes: shapes::all(),
+        }
+    }
+}
 impl App {
+    pub fn new(area: Area, available_universes: Vec<Universe>, poll_t: Duration) -> Self {
+        App {
+            area,
+            universe: available_universes[0].clone(),
+            i: 0,
+            poll_t,
+            paused: false,
+            available_universes,
+        }
+    }
+    pub fn len(&self) -> usize {
+        self.available_universes.len() + shapes::N
+    }
+    #[must_use]
+    pub fn is_empty(&self) -> bool {
+        self.len() == 0
+    }
+    pub fn get(&self) -> Universe {
+        let true_len = self.available_universes.len();
+        if self.i < true_len {
+            self.available_universes
+                .get(self.i)
+                .expect("display area is too small to fit current shape")
+                .clone()
+        } else {
+            shapes::get_special(self.i - true_len, self.area)
+        }
+    }
+
+    // pub fn render_universe(&self) {
+    //     println!("{}", self.universe);
+    // }
+
+    pub fn play_pause(&mut self, prev_poll_t: &mut Duration) {
+        if self.paused {
+            self.poll_t = *prev_poll_t;
+        } else {
+            *prev_poll_t = self.poll_t;
+            self.poll_t = Duration::MAX;
+        }
+        self.paused = !self.paused;
+    }
+    pub fn restart(&mut self) {
+        let univ = self.get();
+        self.universe = Universe::from_figur(self.area, univ).unwrap();
+    }
+
+    pub fn tick(&mut self) {
+        self.universe.tick();
+    }
+
+    pub fn faster(&mut self, big: bool) {
+        if !self.paused {
+            let div = if big { 2 } else { 5 };
+            self.poll_t = self
+                .poll_t
+                .checked_sub(self.poll_t.checked_div(div).unwrap_or(DEF_DUR))
+                .unwrap_or(DEF_DUR);
+        }
+    }
+    pub fn slower(&mut self, big: bool) {
+        if !self.paused {
+            let div = if big { 2 } else { 5 };
+            self.poll_t = self
+                .poll_t
+                .checked_add(self.poll_t.checked_div(div).unwrap_or(DEF_DUR))
+                .unwrap_or(DEF_DUR);
+        }
+    }
+
+    pub fn next(&mut self) {
+        if self.i + 1 == self.len() {
+            self.i = 0;
+        } else {
+            self.i += 1;
+        }
+        self.restart();
+    }
+    pub fn prev(&mut self) {
+        if self.i > 0 {
+            self.i -= 1;
+        } else {
+            self.i = self.len() - 1;
+        }
+        self.restart();
+    }
     pub fn run<B: Backend>(&mut self, terminal: &mut Terminal<B>) -> io::Result<()> {
         let mut prev_poll_t = self.poll_t;
 
@@ -59,111 +165,5 @@ impl App {
         }
 
         Ok(())
-    }
-}
-
-pub struct App {
-    pub universe: Universe,
-    pub i: usize,
-    pub poll_t: Duration,
-    pub paused: bool,
-    pub area: Area,
-}
-impl Default for App {
-    fn default() -> Self {
-        App {
-            area: Area::default(),
-            universe: Universe::default(),
-            i: 0,
-            poll_t: DEF_DUR,
-            paused: false,
-        }
-    }
-}
-impl App {
-    pub fn new(area: Area, universe: Universe, poll_t: Duration) -> Self {
-        App {
-            area,
-            universe,
-            i: 0,
-            poll_t,
-            paused: false,
-        }
-    }
-    // pub fn render_universe(&self) {
-    //     println!("{}", self.universe);
-    // }
-
-    pub fn play_pause(&mut self, prev_poll_t: &mut Duration) {
-        if self.paused {
-            self.poll_t = *prev_poll_t;
-        } else {
-            *prev_poll_t = self.poll_t;
-            self.poll_t = Duration::MAX;
-        }
-        self.paused = !self.paused;
-    }
-    pub fn restart(&mut self) {
-        self.universe = shapes::get(self.area, self.i)
-            .inspect_err(|e| log::error!("{e:?}"))
-            .expect("display area is too small to fit current shape");
-    }
-
-    pub fn tick(&mut self) {
-        self.universe.tick();
-    }
-
-    pub fn faster(&mut self, big: bool) {
-        if !self.paused {
-            let div = if big { 2 } else { 5 };
-            self.poll_t = self
-                .poll_t
-                .checked_sub(self.poll_t.checked_div(div).unwrap_or(DEF_DUR))
-                .unwrap_or(DEF_DUR);
-        }
-    }
-    pub fn slower(&mut self, big: bool) {
-        if !self.paused {
-            let div = if big { 2 } else { 5 };
-            self.poll_t = self
-                .poll_t
-                .checked_add(self.poll_t.checked_div(div).unwrap_or(DEF_DUR))
-                .unwrap_or(DEF_DUR);
-        }
-    }
-
-    pub fn next(&mut self) {
-        if self.i + 1 == shapes::N as usize {
-            self.i = 0;
-        } else {
-            self.i += 1;
-        }
-        if let Ok(shape) = shapes::get(self.area, self.i) {
-            self.universe = shape;
-        } else {
-            log::error!(
-                "couldn't switch to next shape: number of shapes: {}, idx: {}, universe: {:?}",
-                shapes::N,
-                self.i,
-                self.universe
-            );
-        }
-    }
-    pub fn prev(&mut self) {
-        if self.i > 0 {
-            self.i -= 1;
-        } else {
-            self.i = shapes::N as usize - 1;
-        }
-        if let Ok(shape) = shapes::get(self.area, self.i) {
-            self.universe = shape;
-        } else {
-            log::error!(
-                "couldn't switch to previous shape: number of shapes: {}, idx: {}, universe: {:?}",
-                shapes::N,
-                self.i,
-                self.universe
-            );
-        }
     }
 }
